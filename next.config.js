9b3f2121af6222e7dfe7136a139fc190acd0ee63
Yannick Scho <yannick.scho@tobit.com>
2021-08-08 22:18:00 +0200

    :sparkles: Remove next-pwa

--- next.config.js
+++ next.config.js
@@ -1,10 +1,3 @@
-// eslint-disable-next-line @typescript-eslint/no-var-requires
-const withPWA = require('next-pwa');
-
-module.exports = withPWA({
-    pwa: {
-        dest: 'public',
-        disable: process.env.NODE_ENV === 'development',
-    },
+module.exports = {
     reactStrictMode: false,
-});
+};
