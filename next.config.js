d19565c86d4729dc2fba1d0ecd9796912b0ff539
Yannick Scho <yannick.scho@tobit.com>
2021-08-08 13:53:18 +0200

    :sparkles: Add next pwa

--- next.config.js
+++ next.config.js
@@ -1,3 +1,9 @@
-module.exports = {
+// eslint-disable-next-line @typescript-eslint/no-var-requires
+const withPWA = require('next-pwa');
+
+module.exports = withPWA({
+    pwa: {
+        dest: 'public',
+    },
     reactStrictMode: false,
-};
+});
