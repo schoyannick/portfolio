6ae904fc54fc454fca268878cb3cd77632e3ed35
Unknown <scho.yannick@gmail.com>
2021-09-01 22:08:49 +0200

    :bug: Fix header background overflow

--- src/components/header/HeaderStyles.ts
+++ src/components/header/HeaderStyles.ts
@@ -4,17 +4,19 @@ export const StyledHeader = styled.div`
     height: 70px;
     position: fixed;
     top: 0;
-    max-width: 1100px;
-    width: calc(100% - 40px);
+    left: 0;
+    width: 100%;
     z-index: 1;
 `;
 
 export const StyledHeaderMargin = styled.div`
-    width: 100%;
+    width: calc(100% - 40px);
+    max-width: 1100px;
     height: 100%;
     display: flex;
     justify-content: space-between;
     align-items: center;
+    margin: auto;
 `;
 
 export const StyledLogo = styled.div`
