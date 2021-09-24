4f1700f4843589dc6dd2f9700f47c9410a12ef68
Yannick Scho <scho.yannick@gmail.com>
2021-09-24 22:25:40 +0200

    :lipstick: Update mobile page selection menu

--- src/components/global-wrapper/overlay/page-selection/StyledPageSelection.ts
+++ src/components/global-wrapper/overlay/page-selection/StyledPageSelection.ts
@@ -5,6 +5,17 @@ export const StyledPageSelectionWrapper = styled('div')`
 `;
 
 export const StyledPageSelection = styled('div')<{ isSelected: boolean }>`
-    font-size: 18px;
-    margin: 6px;
+    font-size: 25px;
+    margin: 20px 10px;
+    position: relative;
+
+    &:after {
+        content: '';
+        position: absolute;
+        display: block;
+        width: 100%;
+        border-bottom: 1px solid;
+        border-bottom-color: var(--grey-color);
+        bottom: -3px;
+    }
 `;
